f5e62e74ce3a3a8cb16721af0de2c3739c11a8f3
Eduardo Lillo <eduardolillom@gmail.com>
2026-06-29 16:23:18 -0400

    feat: add Valores interface and data structure; update valores.astro to utilize new data

---
+++ src/consts/valores.ts
@@ -0,0 +1,33 @@
+export interface Valores {
+  id: number;
+  title: string;
+  text: string;
+}
+
+export const valores: Valores[] = [
+    {
+        id: 1,
+        title: "La oración",
+        text: "La oración es el primero de los cinco valores centrales de la Iglesia de Dios de la Profecía. La oración lo toca todo, informa todas las actividades, empodera todo el ministerio y el servicio y permea la obra de principio a fin. Jesús dijo: “Escrito está: Mi casa, casa de oración será llamada” (Mateo 21:13). Jesús no da ninguna otra descripción sobre Su casa en ninguna parte. No habla de cortinas ni de muebles ni de instrumentos musicales. El Antiguo Testamento contiene mucha información acerca del aspecto que debían tener el tabernáculo y el templo. Además, describe con gran detalle las actividades que debían tener lugar en esos lugares de adoración. Aquí, Jesús pinta un cuadro para que la iglesia, aún no nacida, vea cómo va a ser Su casa."
+    },
+    {
+        id: 2,
+        title: "La cosecha",
+        text: "En 1994, el Espíritu Santo instruyó claramente a la Iglesia de Dios de la Profecía a “tornarse a la cosecha”. Este fue un momento crítico para nuestro movimiento. Dios nos había hablado y Su llamado nos dirigiría. Las reformas que la Iglesia de Dios de la Profecía ha experimentado desde entonces han solidificado nuestra determinación de permanecer alineados con la prioridad de la cosecha de Dios. Uno de los resultados ha sido el crecimiento del ministerio a nivel mundial. Al presente, la membresía es cuatro veces mayor que la de 1994. Hoy en día, el liderazgo de la Iglesia de Dios de la Profecía mantiene el llamado de la Cosecha de Dios como un valor central y un mandato fundamental. No ha sido rescindido ni tampoco se ha cumplido por completo. Tampoco podrá serlo hasta que todos hayan escuchado."
+    },
+    {
+        id: 3,
+        title: "El desarrollo de liderazgo",
+        text: "El desarrollo del liderazgo es el tercer valor central. La Iglesia de Dios de la Profecía ha invertido considerables recursos en la creación de sistemas que cultiven una cultura de excelencia y produzcan líderes competentes y capacitados. Como resultado, le ha sido posible a la Iglesia generar resultados estables y reproducibles en sus programas de formación de líderes e incorporar la responsabilidad en dichos programas. La Iglesia también ha fortalecido de manera significativa y mensurable la colaboración y la conexión entre los líderes ministeriales."
+    },
+    {
+        id: 4,
+        title: "La mayordomía bíblica",
+        text: "El cuarto valor central de la Iglesia de Dios de la Profecía es la mayordomía bíblica. La Iglesia afirma que todo le pertenece a Dios. Aunque Él ha confiado misericordiosamente al ser humano el cuidado, el desarrollo y la utilización de Sus recursos, los cristianos son responsables de administrar bien Sus posesiones y de hacerlo de acuerdo con Sus deseos y propósitos. La Iglesia reconoce y anima a sus miembros a reconocer que son administradores de los recursos, las capacidades y las oportunidades que Dios les ha confiado, y un día, cada uno será llamado a dar cuenta de cómo se administró lo que el Maestro nos dio. Por lo tanto, es con sumo gozo que la Iglesia trabaja mano a mano con Dios, utilizando lo que Él ha provisto, para Su gloria y honor."
+    },
+    {
+        id: 5,
+        title: "El servicio",
+        text: "El quinto valor central de la Iglesia de Dios de la Profecía es el servicio. Jesús sirvió. Él llama a Sus seguidores a servir. Los líderes, las iglesias y los miembros de la Iglesia de Dios de la Profecía sirven a sus comunidades de muchas maneras alrededor del mundo. Sirven a través de eventos comunitarios, a menudo asociándose con organizaciones de caridad para ayudar a los necesitados. También llevan a cabo campañas de limpieza, ayuda de emergencia cuando ocurre un desastre, ligas deportivas juveniles, programas extraescolares, cuidado de ancianos, bancos de alimentos, a la vez que enseñan, predican, efectúan donaciones, cocinan, limpian, ayudan, proveen transportación… Existen tantas maneras de servir como congregaciones de la Iglesia de Dios de la Profecía. Nuestra gente sirve."
+    }
+]
