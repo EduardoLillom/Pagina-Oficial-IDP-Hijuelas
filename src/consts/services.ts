ba351b24d85bac98c8ccb4c80d59f78c61584fc1
EduardoLillom <eduardolillom@gmail.com>
2026-07-16 20:23:29 -0400

    feat: update service details and correct schedule for clarity

--- src/consts/services.ts
+++ src/consts/services.ts
@@ -7,21 +7,21 @@ export interface Service {
 
 export const services: Service[] = [
   {
-    title: "Servicios de adoración",
-    time: "Jueves, 19:30hrs",
-    description: "Un espacio diseñado para profundizar en la Palabra de Dios para todas las edades.",
-    accent: "border-cyan-500"
-  },
-  {
-    title: "Oración y Doctrina",
-    time: "Miércoles - 7:00 PM",
+    title: "Oración y Discipulado",
+    time: "Jueves - 19:30 hrs",
     description: "Fortaleciendo nuestra fe y comunión con el Espíritu Santo en unidad.",
     accent: "border-emerald-500"
   },
   {
     title: "Ministerios",
-    time: "Viernes, 10:30 hrs",
+    time: "Viernes, 18:00 hrs - 20:00 hrs",
     description: "Alabanza, oración y un mensaje que transformará tu vida y la de tu familia.",
     accent: "border-red-600"
+  },
+  {
+    title: "Servicios de adoración",
+    time: "Domingo, 10:30 hrs",
+    description: "Un espacio diseñado para profundizar en la Palabra de Dios para todas las edades.",
+    accent: "border-cyan-500"
   },
 ];
